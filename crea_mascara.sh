26c644ad08995f2679ca8f1d766aa534fa8d102a
santosg57 <lgs@unam.mx>
2017-09-07 11:40:28 -0500

    Create crea_mascara.sh

---
+++ crea_mascara.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fslmaths -dt float $1 -mul 0 -add 1 -roi 183 1 128 1 84 1 0 1 img_point -odt float
+fslmaths img_point -kernel sphere 5 -fmean img_sphere -odt float
+thresh_it=`fslstats 'img_sphere.nii.gz' -P 10`
+fslmaths img_sphere -thr $thresh_it -bin mascara_bin -odt float
+rm img*
+
